4f2cff9ac8fef49659fb83499d07cb2034da0377
Zhang Hu <iamzhanghu@gmail.com>
2022-09-12 00:32:10 +0800

    comment out events

--- lib/ios/MapView/MarkerManager.m
+++ lib/ios/MapView/MarkerManager.m
@@ -8,10 +8,10 @@ RCT_EXPORT_VIEW_PROPERTY(draggable, BOOL)
 RCT_EXPORT_VIEW_PROPERTY(zIndex, int)
 RCT_EXPORT_VIEW_PROPERTY(icon, NSDictionary)
 
-RCT_EXPORT_VIEW_PROPERTY(onPress, RCTDirectEventBlock)
-RCT_EXPORT_VIEW_PROPERTY(onDragStart, RCTDirectEventBlock)
-RCT_EXPORT_VIEW_PROPERTY(onDrag, RCTDirectEventBlock)
-RCT_EXPORT_VIEW_PROPERTY(onDragEnd, RCTDirectEventBlock)
+// RCT_EXPORT_VIEW_PROPERTY(onPress, RCTDirectEventBlock)
+// RCT_EXPORT_VIEW_PROPERTY(onDragStart, RCTDirectEventBlock)
+// RCT_EXPORT_VIEW_PROPERTY(onDrag, RCTDirectEventBlock)
+// RCT_EXPORT_VIEW_PROPERTY(onDragEnd, RCTDirectEventBlock)
 
 RCT_EXTERN_METHOD(update:(nonnull NSNumber *)reactTag)
 
